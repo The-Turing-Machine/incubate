5b876173e37fd0623a7aabcd60ae438339009098
arsh23 <programmer.arsh@gmail.com>
2016-11-19 15:32:14 +0530

    basic scraper complete

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,9 +1,18 @@
 from lxml import html
 import requests
 import re
+import json
 
-url = ''
+url = 'https://openi.nlm.nih.gov/gridquery.php?q=&it=x,xg&sub=x'
+regex = re.compile(r"var oi = (.*);")
 
 r = requests.get(url)
 tree = html.fromstring(r.text)
-imgs = tree.xpath('//img[contains(@class, "brand-img")]/@src')
+
+script = tree.xpath('//script[@language="javascript"]/text()')[0]
+
+json_string = regex.findall(script)[0]
+json_data = json.loads(json_string)
+
+with open('data.json', 'w') as f:
+    json.dump(json_data, f)
